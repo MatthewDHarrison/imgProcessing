b66fd5ebb6ea7a945da906553050f028b94a6365
MatthewDHarrison <madoha5812@gmail.com>
2022-09-17 19:49:29 +1000

    fixed buggy histogram equalizer, updated README

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Image Processing Mini Problems
 
 This repository will be a collection of mini image processing problems that
-serve to aid in my learning and understanding of computer vision. 
+serve to aid in my learning and understanding of computer vision.
 
 ## Otsu's Method for Computing Threshold Automatically -- otsu.py
 
 Thresholding is a way to separate out shapes in the image based on whether
 certain pixels are above or below a threshold for intensity. The method
-I implemented here searches for a "good" threshold by maximising the
+I implemented here searches for a "good" threshold by maximizing the
 inter-class variance between the two groups created by the threshold.
 Intuitively, this means that the algorithm selects the threshold at which the
 two class created are most different from one another in terms of average pixel
@@ -19,3 +19,19 @@ dependent on the contrast between subject and background, but the technique can
 be quite effective.
 
 To run: python3 otsu.py [imageName]
+
+## Histogram Equalization for Pixel Intensity
+
+This algorithm increases contrast near histogram maxima and decreases contrast
+near histogram minima. This results in a much more equal histogram of pixel
+intensities.
+
+To accomplish this task, I implemented the following transformation:
+    $s_k = T(r_k) = \frac{(L - 1)}{M * N}*\sum_{j=0}^{k}n_j$
+Where:
+ - $r \in [0, L - 1]$ represents pixel values (in intensity) with $r = 0$ being black and $r = L - 1$ being white
+ - $L$ is the number of distinct pixel values, i.e. 256
+ - $M, N$ are the dimensions of the photo
+ - $n_k$ is the number of pixels with intensity $r_k$
+
+To run: python3 histEq.py [imageName]
--- histEq.py
+++ histEq.py
@@ -4,40 +4,61 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 
-figure, (ax0, ax1) = plt.subplots(2, 1)
-print(ax0)
+figure, ax = plt.subplots(2, 2)
+M = 400
+N = 300
+
 if len(sys.argv) > 1:
     image = Image.open(sys.argv[1])
 else:
     image = Image.open('kdot.jpg')
-res_image = image.resize((400,300)).convert('L')
+res_image = image.resize((M,N)).convert('L')
 res_image.show()
 
 im = np.array(res_image, dtype=np.uint8)
-print(im.shape)
 flatIm = im.flatten()
-print(type(im))
+print(len(flatIm))
 
 # array of number of pixels with gray value = index
 n = np.zeros(256)
 for i in range(0, len(flatIm)):
     n[flatIm[i]] += 1
+
 print(n)
-ax0.hist(flatIm, np.arange(0, 256))
-ax0.set_title("Histogram of Pixel Values")
+ax[0][0].hist(flatIm, np.arange(0, 256))
+ax[0][0].set_title("Histogram of Pixel Values")
 
-C = 255 / (300 * 400)
+C = 255 / (N * M)
+print("C is ", C)
 im_copy = im.copy()
-T = np.zeros(256)
+im_histEq = im.copy()
+im_histEq_c = im.copy()
 
+T = np.zeros(256)
+T_c = np.zeros(256)
+g = 0.5
 for k in range(0,256):
     # where pixel intesntiy = k, replace with C * sum from 0 to i of n
-    im_copy[im_copy == k] = C * np.sum(n[0:k])
-    T[k] = C * np.sum(n[0:k])
+    # to constrain slope, find distance between T[k] and k, subtract dist * g,
+    # where g is some fraction penalty
+    T[k] = round(C * np.sum(n[0:k+1]))
+    print("Sum is ", np.sum(n[0:k+1]))
+    print(k, T[k])
+    im_histEq[im == k] = T[k]
+    T_c[k] = T[k] - g * (T[k] - k)
+    im_histEq_c[im == k] = T_c[k]
 
-ax1.plot(np.arange(0, 256), T)
-ax1.set_title("Plot of T(r)")
+ax[0][1].plot(np.arange(0, 256), T)
+ax[0][1].set_title("Plot of T(r)")
+ax[1][1].plot(np.arange(0, 256), T_c)
+ax[1][1].set_title("Plot of T(r) w/ Constrained Slope")
 
+
+ax[1][0].hist(im_histEq.flatten(), np.arange(0, 256))
+ax[1][0].set_title("Histogram of Pixel Values After Equalization")
 plt.show()
-image_histEq = Image.fromarray(im_copy)
+
+image_histEq = Image.fromarray(im_histEq)
 image_histEq.show()
+image_histEq_c = Image.fromarray(im_histEq_c)
+image_histEq_c.show()
